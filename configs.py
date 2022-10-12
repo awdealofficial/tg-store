6c1747cda48ad9fa45b50b8b6c22663a87976ddb
awdealofficial <awdealofficial@gmail.com>
2022-10-12 16:42:35 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME","Freeurlbot")
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001805346080"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "875770605"))
-	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://tiher75183:amanmongodb@1@cluster0.vgtqk3b.mongodb.net/?retryWrites=true&w=majority")
+	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://github:github@cluster0.gwcnv4n.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001668318959")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001547162655")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
